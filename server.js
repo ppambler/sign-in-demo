9d7f15891c681cce8ec33901cdc5c18524506d91
Ambler <allinlbj@gmail.com>
2018-07-10 10:39:47 +0800

    后台拿到用户post的数据

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if(!port){
+if (!port) {
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function (request, response) {
   var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url 
+  var pathWithQuery = request.url
   var queryString = ''
-  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  if (pathWithQuery.indexOf('?') >= 0) {
+    queryString = pathWithQuery.substring(pathWithQuery.indexOf('?'))
+  }
   var path = parsedUrl.pathname
   var query = parsedUrl.query
   var method = request.method
@@ -21,27 +23,33 @@ var server = http.createServer(function(request, response){
 
   console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
 
-  if(path === '/'){
+  if (path === '/') {
     let string = fs.readFileSync('./index.html', 'utf8')
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(string)
     response.end()
-  }else if(path === '/sign_up'){
-    let string = fs.readFileSync('./sign_up.html','utf8')
+  } else if (path === '/sign_up' && method === 'GET') {
+    let string = fs.readFileSync('./sign_up.html', 'utf8')
     response.statusCode = 200
-    response.setHeader('content-Type','text/html;charset=utf-8')
+    response.setHeader('content-Type', 'text/html;charset=utf-8')
     // response.setHeader('Access-Control-Allow-Origin','http://127.0.0.1:5500')
     response.write(string)
     response.end()
 
-  }else if(path==='/main.js'){
+  }else if(path === '/sign_up' && method === 'POST') {
+    readBody(request).then((body)=>{
+      console.log(body)
+      response.statusCode = 200
+      response.end()
+    })
+  } else if (path === '/main.js') {
     let string = fs.readFileSync('./main.js', 'utf8')
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
     response.write(string)
     response.end()
-  }else if(path==='/xxx'){
+  } else if (path === '/xxx') {
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/json;charset=utf-8')
     response.setHeader('Access-Control-Allow-Origin', 'http://frank.com:8001')
@@ -56,7 +64,7 @@ var server = http.createServer(function(request, response){
     }
     `)
     response.end()
-  }else{
+  } else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(`
@@ -70,5 +78,19 @@ var server = http.createServer(function(request, response){
   /******** 代码结束，下面不要看 ************/
 })
 
+function readBody(request) {
+  // 之所以用Promise,那是因为这是异步的
+  return new Promise((resolve, reject) => {
+    let body = [];
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      // at this point, `body` has the entire request body stored in it as a string
+      // 这个回调函数在下一个then中
+      resolve(body)
+    });
+  })
+}
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
